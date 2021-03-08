39a44c81ad6262346ded613c287c9d9cdeae054f
Erik Mikac <mikacew@gmail.com>
2021-03-07 21:26:01 -0500

    Initial Commit

---
+++ Shared/ControllingAnimationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControllingAnimationStackApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ControllingAnimationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
