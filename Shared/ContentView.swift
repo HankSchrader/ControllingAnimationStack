91ff30baf6941f0054475a33f0215f75e7b845c7
Erik Mikac <mikacew@gmail.com>
2021-03-07 21:33:49 -0500

    Day 33, First video.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -3,14 +3,25 @@
 //  Shared
 //
 //  Created by Erik Mikac on 3/7/21.
-//
-
+//Day 33 of Hacking with Swift.
+/*
+ Bottom Line: It MATTERS how you apply your animation modifiers.
+ */
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enabled = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+            self.enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(enabled ? .default : nil)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60: 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+ 
     }
 }
 
